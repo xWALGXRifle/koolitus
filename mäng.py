967b5c402d3649e51b9f279e6df4fc606b103593
xWALGXRifle <maerty.kruusmaa@gamil.com>
2020-04-20 07:54:12 +0300

    jooksin rappa

--- mäng.py
+++ mäng.py
@@ -6,7 +6,7 @@ while kontroll == True:
     klass = input("warrior, defender, berseker, assasin")
     relv = input("axe, swordshield, twohandssword, twoswords")
     turvis = input("leather, light, medium, heavy")
-    if (relv == "warrior" or relv == "defender" or relv == "twohandssword" or relv == "assasin") and (turvis == "leather" or turvis == "light" or turvis == "medium"or turvis == "heavy"):
+    if (klass == "warrior" or klass == "defender" or klass == "berseker" or klass == "assasin") and (relv == "axe" or relv == "swordshield" or relv == "twohandssword" or relv == "twoswords") and (turvis == "leather" or turvis == "light" or turvis == "medium" or turvis == "heavy"):
         kontroll = False
     else:
         print("palun sisesta uuesti")
@@ -28,7 +28,7 @@ Diablo = {
     }
 
 if turvis == "leather":
-    armor= 2
+    armor = 2
 elif turvis == "light":
     armor = 4
 elif turvis == "medium":
@@ -36,21 +36,37 @@ elif turvis == "medium":
 elif turvis == "heavy":
     armor = 25
 
+if klass == "warrior":# siin laheb errorrisse
+     elu = 100
+elif klass == "defender":
+      elu = 150
+elif klass == "berseker":
+      elu = 125
+elif klass == "assasin":
+      elu = 75
 
-
+if relv == "axe":
+    tugevus = 25
+elif relv == "swordshield":
+    tugevus = 14
+elif relv == "twohandssword":
+    tugevus = 30
+elif relv == "twoswords":
+    tugevus = 28
+    
 combat = True
 while combat == True:
     """print ("kasutaja lööb")
     Diablo ["elud"] = Diablo["elud"] - random.randint(0, 10)
     print ("Diblo lööb, pane valm valmis")
     player["elud"] = player["elud"] - random.randint(15, 25)""" #lihtsam versioon
-    if player ["relv"] == "warrior":
+    if player ["klass"] == "warrior":
         dmg = random.randint(12, 25)
-    elif player ["relv"] == "defender":
+    elif player ["klass"] == "defender":
         dmg = random.randint(4, 21) 
-    elif player ["relv"] == "twohandssword":
+    elif player ["klass"] == "berseker":
         dmg = random.randint(9, 20)
-    elif player ["relv"] == "assasin":
+    elif player ["klass"] == "assasin":
         dmg = random.randint(7, 24)
     
     if dmg >= 19:
